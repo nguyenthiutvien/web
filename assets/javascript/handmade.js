2dd1748842d7a386c22c9017b6bd6ccf3a3fcbde
Nguyen Thi Ut Vien <97380870+nguyenthiutvien@users.noreply.github.com>
2022-12-29 00:57:47 +0700

    help me

---
+++ assets/javascript/handmade.js
@@ -0,0 +1,31 @@
+
+import axios from 'axios';
+const URL_handmade= 'https://63a5720d318b23efa793a600.mockapi.io/api/products';
+
+function getData2(){
+    axios.get(`${URL_handmade}`)
+    .then(
+        function (res){
+            showHandmade(res)
+        }
+            )
+}
+function showHandmade(arr){
+    for (let i=0; i< arr.data.lengh; i++){
+        if(arr.data[i].type.for =='handmade'){
+            document.getElementById('products_list-handmade').innerHTML+=`
+            <div class ="products_handmade">
+            <div class ="img_handmade">
+                <img src = "${arr.data[i].image}" alt="">
+            </div>
+            <div>
+                <p>${arr.data[i].name}</p>
+                <p>${arr.data[i].price}</p>
+            <div>
+        </div>
+            `
+        }
+    }
+}
+
+getData2();
